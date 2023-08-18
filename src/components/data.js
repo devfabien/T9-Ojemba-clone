41b2a5baabce5e48ae75dca8acd79623024306c4
devfabien <ish001fabi@gmail.com>
2023-08-18 19:18:14 +0200

    Managed TeamPage #009 adding the rest accordions from part1 adding the maibornwolf logo

---
+++ src/components/data.js
@@ -0,0 +1,34 @@
+const accordion=[
+    {
+        title:"Remote",
+        description:"not ready",
+    },
+    {
+        title:"Englischsprachig",
+        description:"not ready",
+    },
+    {
+        title:"Hohe Qualität ",
+        description:"not ready",
+    },
+    {
+        title:"100% Verantwortung",
+        description:"not ready",
+    },
+    {
+        title:"Riesiger Talentepool ",
+        description:"not ready",
+    },
+    {
+        title:"Risikoarm ",
+        description:"not ready",
+    },
+    
+]
+let acc1=document.getElementById('accordion1');
+console.log("jdfdhjf",accordion);
+acc1.innerHTML=`${accordion.filter((v,i)=>i<2).map((data)=>(` <div class="flex justify-between font-bold font-para px-2 py-3 border-b-2 border-primary w-full"><h2>${data.title}</h2> <h2>+</h2></div>`)).join('')}`
+
+let acc2=document.getElementById('accordion2');
+console.log("jdfdhjf",accordion);
+acc2.innerHTML=`${accordion.filter((v,i)=>i>1).map((data)=>(` <div class="flex justify-between font-bold font-para px-2 py-3 border-b-2 border-primary w-full my-3 "><h2>${data.title}</h2> <h2>+</h2></div>`)).join('')}`
