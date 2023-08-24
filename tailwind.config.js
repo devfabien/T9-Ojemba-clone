72bba9c91dbff162f46febb7da20bcaaf657be11
devfabien <ish001fabi@gmail.com>
2023-08-24 16:09:22 +0200

    Fixing the card #024, #026, #027 #024 : fixing managed team page #026 : fixing the Qualitat section #027 : fixing founder page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        darkPrimary:'#116355',
         primary: "#8ACC50",
         secondary: "#F1F4F6",
         textColorblack: "#000000",
         textColorwhite: "#FFFFFF",
         backgroundColor: "#001342",
+        yellow:"#F4BA00"
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
-        para: ["Montserrat"],
+        para: ["Montserrat","sans-serif"],
         title: ['Raleway','sans-serif']
       },
     },
